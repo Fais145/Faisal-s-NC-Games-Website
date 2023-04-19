0d8f5ce863708b5a31bb0a2f9fd9161e7727bb29
Faisal <faisalmaya@hotmail.com>
2023-04-19 13:42:06 +0100

    set up my vote buttons

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,25 +1,54 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 function ReviewCard({ review }) {
-    const { owner, title, designer, review_img_url, category, created_at, votes, comment_count } = review;
-  
-    return (
-      <div className="review-card">
-        <img src={review_img_url} alt={title} />
-        <div>
-        <Link to={`/reviews/${review.review_id}`}>
-          <h3 className="review-title">{title}</h3>
-          </Link>
-          <p>
-            By {owner} on {new Date(created_at).toLocaleDateString()}
-          </p>
-          <p>
-            Designer: {designer} | Category: {category}
-          </p>
-          <p>Votes: {votes} | Comments: {comment_count}</p>
+  const {
+    owner,
+    title,
+    designer,
+    review_img_url,
+    category,
+    created_at,
+    votes,
+    comment_count,
+  } = review;
+
+  const body = review.review_body ? review.review_body : null;
+
+  const handleUpvote = () => {};
+
+  const handleDownvote = () => {};
+
+  return (
+    <>
+      <div className="review">
+        <div className="review-card">
+          <img src={review_img_url} alt={title} />
+          <div>
+            <Link to={`/reviews/${review.review_id}`}>
+              <h3 className="review-title">{title}</h3>
+            </Link>
+            <p>
+              By {owner} on {new Date(created_at).toLocaleDateString()}
+            </p>
+            <p>
+              Designer: {designer} | Category: {category}
+            </p>
+            <p>
+              Votes: {votes} | Comments: {comment_count}
+            </p>
+          </div>
+          <div className="vote-buttons">
+            <button className="upvote-button" onClick={handleUpvote}>
+              Upvote
+            </button>
+            <button className="downvote-button" onClick={handleDownvote}>
+              Downvote
+            </button>
+          </div>
         </div>
+          {body ? <p className="review-body">{body}</p> : null}
       </div>
-    );
-  }
-  
+    </>
+  );
+}
 
 export default ReviewCard;
