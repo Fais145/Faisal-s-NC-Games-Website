bb8a879d1b3cecf946895d780a4eb69568046e9e
Faisal <faisalmaya@hotmail.com>
2023-04-19 11:21:34 +0100

    added a comment card component and updated my single review page!

---
+++ src/Components/CommentCard.jsx
@@ -0,0 +1,18 @@
+function CommentCard({ comment, user}) {
+  const { body, author, votes, created_at } = comment;
+  const {avatar_url} = user
+  console.log(avatar_url)
+  return (
+    <div className="comment-card">
+      <p className="comment-body">{body}</p>
+      <div className="comment-info">
+        <img src ={avatar_url} alt={`${author}'s profile picture`} />
+        <p className="comment-author">By {author}</p>
+        <p className="comment-votes">Votes: {votes}</p>
+        <p className="comment-date">{new Date(created_at).toLocaleString()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CommentCard;
