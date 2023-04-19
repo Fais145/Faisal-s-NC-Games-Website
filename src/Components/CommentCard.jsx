8080e14d183e142c2c53c82fdaea8696bf5a532a
Faisal <faisalmaya@hotmail.com>
2023-04-19 11:36:31 +0100

    tweaked comment layout

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,13 +1,15 @@
-function CommentCard({ comment, user}) {
+function CommentCard({ comment, user }) {
   const { body, author, votes, created_at } = comment;
-  const {avatar_url} = user
-  console.log(avatar_url)
+  const { avatar_url } = user;
+  console.log(avatar_url);
   return (
     <div className="comment-card">
       <p className="comment-body">{body}</p>
       <div className="comment-info">
-        <img src ={avatar_url} alt={`${author}'s profile picture`} />
-        <p className="comment-author">By {author}</p>
+        <div className="profile-author">
+          <img src={avatar_url} alt={`${author}'s profile picture`} />
+          <p className="comment-author">By {author}</p>
+        </div>
         <p className="comment-votes">Votes: {votes}</p>
         <p className="comment-date">{new Date(created_at).toLocaleString()}</p>
       </div>
