1263c8d61ec8c0ebd037b48d9bc53000299bc6e0
Faisal <faisalmaya@hotmail.com>
2023-04-20 16:12:08 +0100

    refactored my vote state into a like and dislike state, also added a condition to undo likes or dislikes

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { Link } from "react-router-dom";
 import { patchReview } from "../utils/gamesAPI";
+
 function ReviewCard({ review }) {
   const {
     owner,
@@ -19,109 +20,130 @@ function ReviewCard({ review }) {
 
   const [votesForReview, setVotesForReview] = useState(votes);
   const [err, setErr] = useState(null);
-  const [limitReached, setLimitReached] = useState(false);
-  const [voted, setVoted] = useState(null);
-
-  useEffect(() => {
-    const storedVote = localStorage.getItem(`vote_${review_id}`);
-    if (storedVote) {
-      setVoted(storedVote);
-    }
-  }, []);
+  const [liked, setLiked] = useState(localStorage.getItem(`liked:${review_id}`) === 'true');
+  const [disliked, setDisliked] = useState(localStorage.getItem(`disliked:${review_id}`) === 'true');
 
   const handleUpvote = () => {
-    if (voted === "up") {
-      setVotesForReview((previousVotes) => previousVotes - 1);
-      setVoted(null);
-      localStorage.removeItem(`vote_${review_id}`);
-    } else if (voted === "down") {
+    if (disliked) {
       setVotesForReview((previousVotes) => previousVotes + 2);
-      setVoted("up");
-      localStorage.setItem(`vote_${review_id}`, "up");
-    } else {
+      setDisliked(false);
+      setLiked(true);
+      localStorage.setItem(`liked:${review_id}`, true);
+      localStorage.removeItem(`disliked:${review_id}`);
+      
+      patchReview(2, review_id).catch((err) => {
+        setVotesForReview((previousVotes) => previousVotes - 2);
+        setErr("Something went wrong, please try again!");
+        setDisliked(true);
+        setLiked(false);
+        localStorage.setItem(`disliked:${review_id}`, true);
+        localStorage.removeItem(`liked:${review_id}`);
+      });
+    } else if (!liked) {
       setVotesForReview((previousVotes) => previousVotes + 1);
-      setVoted("up");
-      localStorage.setItem(`vote_${review_id}`, "up");
-    }
-
-    patchReview(voted === "up" ? -1 : 1, review_id).catch((err) => {
-      if (voted === "up") {
-        setVotesForReview((previousVotes) => previousVotes + 1);
-      } else if (voted === "down") {
+      setLiked(true);
+      localStorage.setItem(`liked:${review_id}`, true);
+      patchReview(1, review_id).catch((err) => {
         setVotesForReview((previousVotes) => previousVotes - 1);
-      }
-      setErr("Something went wrong, please try again!");
-      setVoted(null);
-    });
+        setErr("Something went wrong, please try again!");
+        setLiked(false);
+        localStorage.removeItem(`liked:${review_id}`);
+      });
+    } else {
+      // User already liked the review, so clicking the button again will undo their vote
+      setVotesForReview((previousVotes) => previousVotes - 1);
+      setLiked(false);
+      localStorage.removeItem(`liked:${review_id}`);
+      patchReview(-1, review_id).catch((err) => {
+        setVotesForReview((previousVotes) => previousVotes + 1);
+        setErr("Something went wrong, please try again!");
+        setLiked(true);
+        localStorage.setItem(`liked:${review_id}`, true);
+      });
+    }
   };
 
   const handleDownvote = () => {
-    if (voted === "down") {
-      setVotesForReview((previousVotes) => previousVotes + 1);
-      setVoted(null);
-      localStorage.removeItem(`vote_${review_id}`);
-    } else if (voted === "up") {
+    if (liked) {
       setVotesForReview((previousVotes) => previousVotes - 2);
-      setVoted("down");
-      localStorage.setItem(`vote_${review_id}`, "down");
-    } else {
+      setLiked(false);
+      setDisliked(true);
+      localStorage.setItem(`disliked:${review_id}`, true);
+      localStorage.removeItem(`liked:${review_id}`);
+      patchReview(-2, review_id).catch((err) => {
+        setVotesForReview((previousVotes) => previousVotes + 2);
+        setErr("Something went wrong, please try again!");
+        setLiked(true);
+        setDisliked(false);
+        localStorage.setItem(`liked:${review_id}`, true);
+        localStorage.removeItem(`disliked:${review_id}`);
+      });
+    } else if (!disliked) {
       setVotesForReview((previousVotes) => previousVotes - 1);
-      setVoted("down");
-      localStorage.setItem(`vote_${review_id}`, "down");
-    }
-
-    patchReview(voted === "down" ? 1 : -1, review_id).catch((err) => {
-      if (voted === "up") {
-        setVotesForReview((previousVotes) => previousVotes - 1);
-      } else if (voted === "down") {
+      setDisliked(true);
+      localStorage.setItem(`disliked:${review_id}`, true);
+      patchReview(-1, review_id).catch((err) => {
         setVotesForReview((previousVotes) => previousVotes + 1);
-      }
-      setErr("Something went wrong, please try again!");
-      setVoted(null);
-    });
+        setErr("Something went wrong, please try again!");
+        setDisliked(false);
+        localStorage.removeItem(`disliked:${review_id}`);
+      });
+    }
+    else {
+      // User already liked the review, so clicking the button again will undo their vote
+      setVotesForReview((previousVotes) => previousVotes + 1);
+      setDisliked(false);
+      localStorage.removeItem(`disliked:${review_id}`);
+      patchReview(1, review_id).catch((err) => {
+        setVotesForReview((previousVotes) => previousVotes - 1);
+        setErr("Something went wrong, please try again!");
+        setDisliked(true);
+        localStorage.setItem(`disliked:${review_id}`, true);
+      });
+    }
   };
 
-  return (
-    <section>
-      <div className="review">
-        <div className="review-card">
-          <img src={review_img_url} alt={title} />
-          <div>
-            <Link to={`/reviews/${review.review_id}`}>
-              <h3 className="review-title">{title}</h3>
-            </Link>
-            <p>
-              By {owner} on {new Date(created_at).toLocaleDateString()}
-            </p>
-            <p>
-              Designer: {designer} | Category: {category}
-            </p>
-            <p>
-              Votes: {votesForReview} | Comments: {comment_count}
-            </p>
+    return (
+      <section>
+        <div className="review">
+          <div className="review-card">
+            <img src={review_img_url} alt={title} />
+            <div>
+              <Link to={`/reviews/${review.review_id}`}>
+                <h3 className="review-title">{title}</h3>
+              </Link>
+              <p>
+                By {owner} on {new Date(created_at).toLocaleDateString()}
+              </p>
+              <p>
+                Designer: {designer} | Category: {category}
+              </p>
+              <p>
+                Votes: {votesForReview} | Comments: {comment_count}
+              </p>
+            </div>
           </div>
+          <div className="vote-buttons">
+            <button
+              className={`upvote-button ${liked ? "liked" : ""}`}
+              onClick={handleUpvote}
+            >
+              {liked ? <p>LIKED</p> : <p>like</p>}
+            </button>
+            <button
+              className={`downvote-button ${
+                disliked ? "disliked" : ""
+              }`}
+              onClick={handleDownvote}
+            >
+              {disliked ? <p>DISLIKED</p> : <p>dislike</p>}
+            </button>
+          </div>
+          {err ? <p>{err}</p> : null}
+          {body ? <p className="review-body">{body}</p> : null}
         </div>
-        <div className="vote-buttons">
-          <button
-            className={`upvote-button ${voted === "up" ? "voted-up" : ""}`}
-            onClick={handleUpvote}
-          >
-            {voted === "up" ? <p>UPVOTED</p> : <p>upvote</p>}
-          </button>
-          <button
-            className={`downvote-button ${
-              voted === "down" ? "voted-down" : ""
-            }`}
-            onClick={handleDownvote}
-          >
-            {voted === "down" ? <p>DOWNVOTED</p> : <p>downvote</p>}
-          </button>
-        </div>
-        {err ? <p>{err}</p> : null}
-        {body ? <p className="review-body">{body}</p> : null}
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  }
 
-export default ReviewCard;
+  export default ReviewCard;
